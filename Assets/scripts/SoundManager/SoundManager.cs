b8582751697cebfee5d82e4f220e03c7e7e56f9f
Muhammed Hadhil <hadhilnjrbrototype@gmail.com>
2023-11-29 20:31:05 +0530

    some corrections

--- Assets/scripts/SoundManager/SoundManager.cs
+++ Assets/scripts/SoundManager/SoundManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Unity.VisualScripting.Dependencies.Sqlite.SQLite3;
 
 public class SoundManager : MonoBehaviour
 {
